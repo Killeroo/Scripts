4d808f59a073cb8314f3601608637be1399eb2c9
Killeroo <matthewcarney64@gmail.com>
2018-07-25 18:41:18 +0100

    Updated build_projects.bat

--- build_projects.bat
+++ build_projects.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
-:: check if git is installed
+SET origin=%~dp0
+set msbuildPath="%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\"
 
-SET origin=%CD%
-
+:main
 FOR /d %%i in (%origin%\*) DO (
 	cd "%%i"
 	cd
@@ -13,23 +13,37 @@ FOR /d %%i in (%origin%\*) DO (
 
 )
 cd %origin%
-
-pause
-
-:main
-
 GOTO:eof
 
 :msbuild
+:: Set .NET 4.6 framework path
+
+SET projectpath=
+:: Save starting directory
+set origDir=%~dp0
+
+:: Goto msbuild dir
+cd %msbuildPath%
+
+:: Run build command
+msbuild.exe %projectpath% /p:configuration=release
+
 GOTO:eof
 
 :netcorebuild
 GOTO:eof
 
 :gobuild
+:: https://golang.org/doc/install/source#environment
+set GOARCH=amd64
+set GOOS=windows
+go tool dist install -v pkg/runtime
+go install -v -a std
 GOTO:eof
 
 :copyBuildFiles
+GOTO:eof
 
 :sendBuildFiles
+BasicTransfer --send
 GOTO:eof
