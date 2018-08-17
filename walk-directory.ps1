c8d69595648a46bccc9f905e49e54334868a5ac9
Killeroo <matthewcarney64@gmail.com>
2018-08-17 17:40:07 +0100

    Removed ip_ranger.ps1 Added walk-directory.ps1

---
+++ walk-directory.ps1
@@ -0,0 +1,21 @@
+﻿# Source: https://stackoverflow.com/a/8024326
+function Walk-Directory($path = $pwd, [string[]]$exclude)
+{
+    foreach ($item in Get-ChildItem $path)
+    {
+        # Ignore certain files and types
+        if ($exclude | Where {$item -like $_}) { continue }
+
+        if (Test-Path $item.FullName -PathType Container) {
+            # If folder
+            Write-Host("[-] " + $item.FullName)
+            Walk-Directory $item.FullName $exclude
+        }
+        else {
+            # If file
+            Write-Host("- " +$item.FullName)
+        }
+    }
+}
+
+Walk-Directory F:\ *.txt,*.bat,"Scripts",9_Populate_Documents.py
