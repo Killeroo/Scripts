4ea5b6c9f3b62a8f17f667056b0b891adad4cc4d
Killeroo <matthewcarney64@gmail.com>
2018-08-22 23:13:53 +0100

    More fleshing out of scrape-files.ps1

--- scrape-files.ps1
+++ scrape-files.ps1
@@ -1,5 +1,14 @@
-﻿# Source: https://stackoverflow.com/a/8024326
-function Walk-Directory($path = $pwd, [string[]]$exclude)
+﻿# Binary detection: https://stackoverflow.com/a/1078277
+
+param (
+    [string]$root = "C:\",
+    [string[]]$scrape = ".txt",
+    [string[]]$excludes = ".py",
+    [string]$path = "results.txt"
+)
+
+# Source: https://stackoverflow.com/a/8024326
+function Walk-Directory($path = $pwd, [string[]]$exclude, [string[]]$extract)
 {
     foreach ($item in Get-ChildItem $path)
     {
@@ -9,11 +18,18 @@ function Walk-Directory($path = $pwd, [string[]]$exclude)
         if (Test-Path $item.FullName -PathType Container) {
             # If folder
             Write-Host("[-] " + $item.FullName)
-            Walk-Directory $item.FullName $exclude
+            Walk-Directory $item.FullName $exclude $extract
         } else {
             # If file
-            Write-Host("- " + $item.FullName)
-            Extract-Content $item.FullName
+            Write-Host("- " + $item.FullName) -NoNewline 
+
+            # Check if we want to extract file type
+            if ($extract | Where {$item.Extension -like $_}) {
+                Write-Host " [*]"
+                Extract-Content $item.FullName
+            } else {
+                Write-Host
+            }
         }
     }
 }
@@ -21,8 +37,13 @@ function Walk-Directory($path = $pwd, [string[]]$exclude)
 Function Extract-Content($path)
 {
     #https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-content?view=powershell-6
-    Write-Output("`r`n####################`r`n" + $path + "`r`n####################`r`n") | Out-File -Encoding string -Append results.txt
-    Get-Content -Path $path -Encoding String | Out-File -Encoding string -Append results.txt
+    try {
+        Write-Output("`r`n####################`r`n" + $path + "`r`n####################`r`n") | Out-File -Encoding string -Append results.txt
+        Get-Content -Path $path -Encoding String | Out-File -Encoding string -Append "$resultsPath$([IO.Path]::DirectorySeparatorChar)results.txt"
+    } catch {
+        Write-Warning("Problem occured during scraping: " + $path)
+    }
 }
 
-Walk-Directory C:\ic-utils 9_Populate_Documents.py,*.exe,*.dll,*.msi,*.mui,*.sdi,*.wim
+Set-Location 
+Walk-Directory $root $excludes $scrape
