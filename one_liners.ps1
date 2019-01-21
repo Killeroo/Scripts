ee9e0cbf9b48733e85f89b7da74095fa05af2ae2
Killeroo <matthewcarney64@gmail.com>
2019-01-21 17:40:47 +0000

    Added one_liners.ps1

---
+++ one_liners.ps1
@@ -0,0 +1,13 @@
+﻿# Collection of powershell one-liners
+
+# Look for string in all files of a the current directory (and sub-directories)
+Get-ChildItem -recurse | select-string -pattern "search_string" | group path | select name
+
+# Search for files names in current directory (and sub-directories)
+get-childitem -include *.sh -recurse
+
+# Lets you change the console cursors position
+$pos = $host.UI.RawUI.CursorPosition; $pos.x += 5; $host.UI.RawUI.CursorPosition = $pos;
+
+# Show all properties of an object
+Get-WmiObject win32_computersystem | Format-List * -Force
