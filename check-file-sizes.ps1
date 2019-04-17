9a81bdb28fc22c583b34ef78cb844235be2fe048
Killeroo <matthewcarney64@gmail.com>
2019-04-17 14:11:38 +0100

    Added check-file-sizes.ps1

---
+++ check-file-sizes.ps1
@@ -0,0 +1,26 @@
+﻿param (
+    [Parameter(Mandatory=$true)][string]$root = "C:\",
+    [int]$size = 200mb
+)
+
+function Walk-Directory($path = $pwd)
+{
+    foreach ($item in Get-ChildItem $path)
+    {
+        if (Test-Path $item.FullName -PathType Container) {
+            # Recurse back into any directories that we find
+            Walk-Directory $item.FullName
+        } else {
+            if ((Get-Item $item.FullName).Length -lt $size) {
+                Write-Warning("[-] " + $item.FullName + " " + [math]::Round((Get-Item $item.FullName).Length / 1mb, 2) + "mb")
+            }
+        }
+    }
+}
+
+function Check-File-Size($path)
+{
+    
+}
+
+Walk-Directory $path
