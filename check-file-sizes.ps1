d28e9a18994ed689ff58569def7869d9a0010387
Killeroo <matthewcarney64@gmail.com>
2019-04-18 14:05:23 +0100

    Polished up check-file-sizes.ps1 script

--- check-file-sizes.ps1
+++ check-file-sizes.ps1
@@ -1,26 +1,36 @@
 ﻿param (
     [Parameter(Mandatory=$true)][string]$root = "C:\",
-    [int]$size = 200mb
+    [Parameter(Mandatory=$true)][int]$size = 200mb,
+    [Parameter(Mandatory=$true)[string[]]$include = (".php", ".gem")
 )
+
+$Global:fileSizeLimit = ($size * 1024 * 1024)
 
 function Walk-Directory($path = $pwd)
 {
+    # Loop through everything in the folder 
     foreach ($item in Get-ChildItem $path)
     {
+        # Check if its a folder, or a file
         if (Test-Path $item.FullName -PathType Container) {
             # Recurse back into any directories that we find
             Walk-Directory $item.FullName
         } else {
-            if ((Get-Item $item.FullName).Length -lt $size) {
-                Write-Warning("[-] " + $item.FullName + " " + [math]::Round((Get-Item $item.FullName).Length / 1mb, 2) + "mb")
-            }
+            # Only check files of a certain type
+            if (!$include.Contains($item.Extension)) { continue }
+
+            Check-File-Size $item
         }
     }
 }
 
-function Check-File-Size($path)
+function Check-File-Size($filePath)
 {
-    
+    if ((Get-Item $filePath.FullName).Length -lt $fileSizeLimit) {
+        Write-Host("[" + [math]::Round((Get-Item $filePath.FullName).Length / 1mb, 2) + "mb] ") -ForegroundColor Red -NoNewline
+        Write-Host($filePath.FullName)
+    }
 }
 
+Write-Host "Searching for files with extensions ($($include)) that are smaller than $($fileSizeLimit/1mb)mb..."
 Walk-Directory $path
